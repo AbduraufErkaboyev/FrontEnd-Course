1bffb1e3d7bae9c3d26ec64610ac6b203aed7b6e
AbduraufErkaboyev <abdurauf14082005@gmail.com>
2022-04-08 13:43:40 +0500

    s

---
+++ JavaScript/1-dars/script.js
@@ -0,0 +1,33 @@
+// var num = 17;
+// var str = "JavaScript";
+
+// console.log(str)
+// console.log(num)    
+
+// let a = 7;
+// let b = 12;
+
+// console.log(a + b)
+
+// const x = 20;
+
+// // x = 23; // ERROR
+
+
+// console.log(`Hello ${a} and ${str}`)
+
+
+// const x = Number(prompt("birinchi sonni kiriting"));
+// const y = Number(prompt("ikkinchi sonni kiriting"));
+
+// console.log(`Sum: ${x + y} \nSybtract: ${x - y} \nMultiply: ${x * y} \nDivide: ${x / y}`)
+
+
+
+let ism = prompt("Ismingizni kiriting:")
+let familiya = prompt("Familiyangizni kiriting:")
+let yosh = prompt("Yoshingizni kiriting:")
+let manzil = prompt("Manzilingizni kiriting:")
+let uqish = prompt("Qayerda o'qiysiz ?")
+
+console.log(`${familiya} ${ism}  ${yosh}da. U ${manzil}da yashaydi. ${uqish}da o'qiydi.`)
